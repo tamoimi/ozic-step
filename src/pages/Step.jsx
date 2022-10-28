b6be4de4a8f558cd21a3a9c4825ed30b64557a10
tami <xorud7979@gmail.com>
2022-10-28 13:39:42 +0900

    feat: step file 추가

---
+++ src/pages/Step.jsx
@@ -0,0 +1,13 @@
+import { Stack } from "@mui/system";
+import React from "react";
+import Step1 from "./Step1";
+import Step2 from "./Step2";
+
+const Step = () => {
+  return (
+    
+      <Step1/>
+    
+  );
+};
+export default Step;
