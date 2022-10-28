d4b704c3ffd01707d6b0373e6f56994b8c4b81ea
tami <xorud7979@gmail.com>
2022-10-28 11:41:59 +0900

    feat: react-route 삭제

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,15 +3,21 @@ import React from "react";
 import { Navigation } from "./Navigation";
 import StepBody from "./StepBody";
 import StepHeader from "./StepHeader";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Step2Body from "./Step2Body";
 
 const Container = () => {
   return (
     <Stack direction="row" spacing={3}>
       <Navigation />
-    <Stack spacing={3}>
-      <StepHeader />
-      <StepBody />
-    </Stack>
+      <Stack spacing={3}>
+        <StepHeader />
+        {/* <StepBody /> */}
+        <Routes>
+          <Route path="/" element={<StepBody />} />
+          <Route path="/Step2Body" element={<Step2Body />} />
+        </Routes>
+      </Stack>
     </Stack>
   );
 };
