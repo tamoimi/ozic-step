f1aeefebf212012d97c918bce31d1525e1e5869a
tami <xorud7979@gmail.com>
2022-10-31 17:17:13 +0900

    Feat: 모달창 open

--- src/components/Merchants/Step1.jsx
+++ src/components/Merchants/Step1.jsx
@@ -6,16 +6,27 @@ import { FormControlLabel } from "@mui/material";
 import { Radio } from "@mui/material";
 import { TextField } from "@mui/material";
 import { Stack } from "@mui/system";
+import PostCode from "../PostCode";
+import { useState } from "react";
 
 const Step1 = ({ setCurrentStepProp }) => {
+  const [openPostCode, setOpenPostCode] = useState(false);
+
+  // React-Hook-Form
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  // 다음단계로 클릭했을 때 한페이지씩 이동
   const onSubmit = (data) => {
     console.log("step1다음으로 클릭 됨");
     setCurrentStepProp((prev) => prev + 1);
+  };
+
+  const openPostCodeModal = () => {
+    setOpenPostCode(true);
   };
 
   return (
@@ -36,7 +47,7 @@ const Step1 = ({ setCurrentStepProp }) => {
             <Stack direction="row">
               <label>가맹점</label>
               <TextField
-                id="franchisee-id"
+                id="franchisee"
                 label="가맹점"
                 variant="outlined"
                 color="primary"
@@ -66,52 +77,77 @@ const Step1 = ({ setCurrentStepProp }) => {
             <Stack direction="row">
               <label>사업자등록번호</label>
               <TextField
-                id="registNum-id"
+                id="registNum"
                 label="사업자등록번호"
                 variant="outlined"
                 type="number"
                 color="primary"
                 {...register("registNum")}
               />
-              {errors.registNum && <span>꼭 필요!</span>} <br />
+              {errors.registNum && <span>꼭 필요!</span>}
             </Stack>
 
             <Stack direction="row">
               <label>가맹점 주소</label>
               <TextField
-                id="franchiseeAd-id"
+                id="franchiseeAd"
                 label="가맹점주소"
                 variant="outlined"
                 color="primary"
                 {...register("franchiseeAd")}
               />
-              {errors.franchisee && <span>This field is required</span>} <br />
+              {errors.franchisee && <span>This field is required</span>}
+              <Button
+                variant="contained"
+                sx={{ background: "#eee", marginLeft: "20px" }}
+                size="medium"
+                onClick={openPostCodeModal}
+              >
+                우편번호
+              </Button>
+              <br />
             </Stack>
+
+            <Stack direction="row">
+              <label></label>
+              <TextField
+                id="franchiseeAd"
+                label="상세주소"
+                variant="outlined"
+                color="primary"
+                {...register("franchiseeAd")}
+              />
+            </Stack>
+
+            <PostCode
+              openPostCode={openPostCode}
+              setOpenPostCode={setOpenPostCode}
+            />
 
             <Stack direction="row">
               <label>가맹점 전화번호</label>
               <TextField
-                id="franchiseeNum-id"
+                id="franchiseeNum"
                 label="전화번호"
                 variant="outlined"
                 type="number"
                 color="primary"
                 {...register("franchiseeNum")}
               />
-              {errors.franchisee && <span>This field is required</span>} <br />
+              {errors.franchisee && <span>This field is required</span>}
             </Stack>
 
             <Stack direction="row">
               <label>가맹점 팩스번호</label>
               <TextField
-                id="franchiseeFaxNum-id"
+                id="franchiseeFaxNum"
                 label="팩스번호"
                 variant="outlined"
                 type="number"
                 color="primary"
                 {...register("franchiseeFaxNum")}
               />
-              {errors.franchisee && <span>This field is required</span>} <br />
+              {errors.franchisee && <span>This field is required</span>}
             </Stack>
           </Stack>
 
@@ -129,6 +165,7 @@ const Step1 = ({ setCurrentStepProp }) => {
       <style>{`
           label {
             width: 150px;
+            line-height: 50px;
           }
           input::-webkit-outer-spin-button,
           input::-webkit-inner-spin-button {
