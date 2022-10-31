ee1558ac912370393559930315b285a845a281bd
tami <xorud7979@gmail.com>
2022-10-31 17:16:37 +0900

    Feat: PostCode 컴포넌트 생성 및 부모 컴포넌트에서 props 받아오기

--- src/components/PostCode.jsx
+++ src/components/PostCode.jsx
@@ -1,10 +1,48 @@
+import React from "react";
+import DaumPostcodeEmbed from "react-daum-postcode";
+import { Modal } from "@mui/material";
+import { Box } from "@mui/system";
 
-const PostCode = () => {
-    return (
-<>
+const PostCode = ({ openPostCode, setOpenPostCode }) => {
+  //useState
+  const handleClose = () => setOpenPostCode(false);
 
-</>
-    )
-}
+  //handler
+  const handle = {
+    //주소 선택 이벤트
+    selectAddress: (data) => {
+      console.log(`
+        주소: ${data.address},
+        우편번호: ${data.zonecode}    
+        `);
+      setOpenPostCode(false);
+    },
+  };
 
-export default PostCode
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <>
+      <Modal open={openPostCode} onClose={handleClose}>
+        <Box sx={style}>
+          <DaumPostcodeEmbed
+            onComplete={handle.selectAddress}
+            autoClose={false}
+          />
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default PostCode;
