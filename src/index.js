37e71fe71ca7a38c2000745faf409acc56b1c2ac
tami <xorud7979@gmail.com>
2022-11-07 10:55:42 +0900

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { FormSample } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <FormSample />
-  </StrictMode>
+  </React.StrictMode>
 );
