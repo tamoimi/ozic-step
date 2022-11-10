4a28b878c447f7fb3324580b40db83edd6f75f43
tami <xorud7979@gmail.com>
2022-11-10 09:10:37 +0900

    Feat: 임의의 가맹 등록 현황 페이지 추가

---
+++ src/pages/FranchiseesList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Paper } from "@mui/material";
+
+const FranchiseesList = () => {
+  return (
+    <>
+      <Paper
+        sx={{
+          width: 240,
+          height: 889,
+          textAlign: "center",
+          borderRadius: 4,
+          p: 3,
+        }}
+      >
+        가맹점 현황 페이즤
+      </Paper>
+    </>
+  );
+};
+
+export default FranchiseesList;
